c4d7c9008145897a8dcc3e4d001a2dc78e134bf6
kakugb <karamat.4101@gmail.com>
2023-07-15 21:12:53 +0500

    New File Add

--- fifteen.js
+++ fifteen.js
@@ -5,5 +5,5 @@ console.log("\t" + frdName[4] + "Who can't make it.\n");
 frdName.pop();
 frdName.push(new_invitee);
 for (var i = 0; i < frdName.length; i++) {
-    console.log("Mr." + frdName[i] + " you are invited for dinner in my home!");
+    console.log("Mr." + frdName[i] + " youu are invited for dinner in my home!");
 }
